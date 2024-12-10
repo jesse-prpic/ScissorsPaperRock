af576faaa07d6098cee46446dc7ce32e817b006a
jesse-prpic <13812@coderacademy.edu.au>
2024-12-10 22:24:06 +1000

    feat: implemented the button click events for the game to work

---
+++ src/scripts/scissorsPaperRock.js
@@ -0,0 +1,52 @@
+// get elements
+
+const scissorsButton = document.getElementById("scissors");
+const paperButton = document.getElementById("paper");
+const rockButton = document.getElementById("rock");
+const resultDiv = document.getElementById("result");
+
+// get the computer choice
+function getComputerChoice(choice) {
+    const choices = ["Scissors", "Paper", "Rock"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+
+}
+function getPlayerChoice(choice) {
+    const choices = ("Scissors", "Paper", "Rock");
+}
+
+// console.log(getComputerChoice());
+
+
+// write the logic for the winner
+function determineWinner(userChoice, computerChoice){
+    if (userChoice === computerChoice){
+        return "It's a tie"
+    } else if (
+        (userChoice === "Scissors" && computerChoice === "Paper") ||
+        (userChoice === "Paper" && computerChoice === "Rock") ||
+        (userChoice === "Rock" && computerChoice === "Scissors")
+    ) {
+        return "You win!";
+    } else {
+        return "Computer wins!";
+    }
+}
+
+// listen to the button click events
+scissorsButton.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner('Scissors', computerChoice);
+    resultDiv.textContent = `You chose Scissors. Computer chose ${computerChoice}. ${result}`
+});
+paperButton.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner('Paper', computerChoice);
+    resultDiv.textContent = `You chose Paper. Computer chose ${computerChoice}. ${result}`
+});
+rockButton.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner('Rock', computerChoice);
+    resultDiv.textContent = `You chose Rock. Computer chose ${computerChoice}. ${result}`
+});
